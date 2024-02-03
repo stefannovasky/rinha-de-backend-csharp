39dd3db8559097604e346ce7cc0c76bed34c9f5c
stefannovasky <stefannovasky@gmail.com>
2024-02-03 20:41:58 -0300

    random number sql command

--- src/Rinha.Web/Program.cs
+++ src/Rinha.Web/Program.cs
@@ -22,13 +22,15 @@ app.MapGet("/", async (ILogger<Program> logger) =>
     await using var dataSource = NpgsqlDataSource.Create(connString);
     await using var connection = await dataSource.OpenConnectionAsync();
 
-    await using var command = new NpgsqlCommand("SELECT 1;", connection);
+
+    var sqlCommand = "SELECT FLOOR(RANDOM()*(1000 - 1 + 1)) + 1 As random_val;";
+    await using var command = new NpgsqlCommand(sqlCommand, connection);
     await using var reader = await command.ExecuteReaderAsync();
 
     string result = "";
     while (await reader.ReadAsync())
     {
-        result = reader.GetInt32(0).ToString();
+        result = reader.GetFloat(0).ToString();
     }
 
     logger.LogInformation("received request");
