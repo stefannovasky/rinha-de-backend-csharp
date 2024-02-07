e5d9ee885760b786f9e4ea3b1f0da865ea0c8c9b
stefannovasky <stefannovasky@gmail.com>
2024-02-06 21:36:33 -0300

    mudei o ddl e tenho que melhorar o endpoint de cadastro

--- init.sql
+++ init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE clientes (
 	id SERIAL PRIMARY KEY,
-	nome VARCHAR(50) NOT NULL,
-	limite INTEGER NOT NULL
+	nome VARCHAR(50) NOT NULL, -- nem precisa mas ta aí
+	limite INTEGER NOT NULL,
+	saldo INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE transacoes (
@@ -15,25 +16,32 @@ CREATE TABLE transacoes (
 		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
 
-CREATE TABLE saldos (
-	id SERIAL PRIMARY KEY,
-	cliente_id INTEGER NOT NULL,
-	valor INTEGER NOT NULL,
-	CONSTRAINT fk_clientes_saldos_id
-		FOREIGN KEY (cliente_id) REFERENCES clientes(id)
-);
+CREATE TYPE resultado_transacao as (cliente_novo_saldo integer, cliente_limite integer);
 
-DO $$
+CREATE FUNCTION criar_transacao(cliente_id INTEGER, valor_transacao INTEGER, tipo_transacao TEXT, descricao_transacao TEXT)
+RETURNS SETOF resultado_transacao
+LANGUAGE plpgsql
+AS $BODY$
+	DECLARE cliente_novo_saldo INTEGER;
+  DECLARE cliente_limite INTEGER;
 BEGIN
-	INSERT INTO clientes (nome, limite)
-	VALUES
-		('eles brigam', 1000 * 100),
-		('por instinto', 800 * 100),
-		('nós criamos', 10000 * 100),
-		('por amor', 100000 * 100),
-		('rinha de galo frases', 5000 * 100);
+	update clientes
+	set saldo = saldo + valor_transacao
+	where id = cliente_id and saldo + valor_transacao >= (-limite)
+	returning saldo, limite into cliente_novo_saldo, cliente_limite;
 
-	INSERT INTO saldos (cliente_id, valor)
-		SELECT id, 0 FROM clientes;
+	if cliente_novo_saldo is null then return; end if;
+
+	insert into transacoes (cliente_id, valor, tipo, descricao)
+	values (cliente_id, valor_transacao, tipo_transacao, descricao_transacao);
+
+	return query select cliente_novo_saldo, cliente_limite;
 END;
-$$;
+$BODY$;
+
+INSERT INTO clientes (nome, limite) values
+	('eles brigam', 1000 * 100),
+	('por instinto', 800 * 100),
+	('nós criamos', 10000 * 100),
+	('por amor', 100000 * 100),
+	('rinha de galo frases', 5000 * 100);
--- src/Rinha.Web/Program.cs
+++ src/Rinha.Web/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 
 // sem criticancia
-var connString = "Host=postgres-db;Username=root;Password=root;Database=rinha-db";
+var connString = "Host=postgres-db;Username=root;Password=root;Database=rinha-db;MaxPoolSize=20;MinPoolSize=5;Connection Pruning Interval=1;Connection Idle Lifetime=2;Enlist=false;No Reset On Close=true";
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,31 +17,45 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.MapPost("/clientes/{id}/transacoes", async (
     [FromRoute] int id,
     [FromBody] CriarTransacao transacao) =>
 {
+    // o certo seria verificar a existencia no banco, mas né, tamo aí
+    if (id > 5 || id < 1)
+    {
+        return Results.NotFound();
+    }
+
     if (!transacao.EhValido())
     {
         return Results.StatusCode(422);
     }
 
+    var valorTransacao = transacao.Tipo == "c" ? int.Parse(transacao.Valor) : - int.Parse(transacao.Valor);
+
     await using var dataSource = NpgsqlDataSource.Create(connString);
     await using var conn = await dataSource.OpenConnectionAsync();
 
-    await using var command = new NpgsqlCommand("select 1 from clientes c where c.id = @Id", conn);
-    command.Parameters.AddWithValue("Id", id);
+    var sql = "select * from criar_transacao(@ClienteId, @ValorTransacao, @TipoTransacao, @DescricaoTransacao);";
+    await using var command = new NpgsqlCommand(sql, conn);
+    command.Parameters.AddWithValue("ClienteId", id);
+    command.Parameters.AddWithValue("ValorTransacao", valorTransacao);
+    command.Parameters.AddWithValue("TipoTransacao", transacao.Tipo);
+    command.Parameters.AddWithValue("DescricaoTransacao", transacao.Descricao);
     await using var reader = await command.ExecuteReaderAsync();
 
-    var clienteExiste = await reader.ReadAsync();
-    if (!clienteExiste)
+    var transacaoTeveSucesso = await reader.ReadAsync();
+    if (!transacaoTeveSucesso)
     {
-        return Results.NotFound();
+        return Results.StatusCode(422);
     }
 
-    return Results.Ok(new { id = id });
+    var novoSaldo = reader["cliente_novo_saldo"];
+    var limite = reader["cliente_limite"];
+    return Results.Ok(new { limite, saldo = novoSaldo });
 });
+
+
 
 app.Run();
